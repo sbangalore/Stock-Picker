f6bdf3d4ae821215e6e37bb4aa85957d857a7af7
Sudarshan <bsudarshan@outlook.com>
2016-12-02 16:49:31 -0500

    finished program

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,4 +1,8 @@
 def stock_picker(stock_prices)
+    #error handling for no prices
+    if not stock_prices.any?
+        return "no prices"
+    end
     #initial test
     max, min = stock_prices.max, stock_prices.min
     max_index, min_index = stock_prices.index(max), stock_prices.index(min)
@@ -36,3 +40,5 @@ puts stock_picker([25,2,10,9]).to_s #expect 1,2
 puts stock_picker([10,12,5,3,20,1,9,20]).to_s #expect 5,7
 puts stock_picker([10,9,8,7,6]).to_s #expect 0,0
 puts stock_picker([18,17,17,16,15]).to_s #expect 0,0
+puts stock_picker([1]).to_s
+puts stock_picker([]).to_s
